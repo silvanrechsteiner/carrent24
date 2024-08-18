83cc7274aa2bdb53fd79584dcdb142039251f9e7
silvanrechsteiner <silvanrechsteiner@bluewin.ch>
2024-08-18 22:39:10 +0200

    initial common abstraction

---
+++ source/src/CarRent/Domain/Common/Entity.cs
@@ -0,0 +1,37 @@
+﻿namespace CarRent.Domain.Common
+{
+    public abstract class Entity : IEquatable<Entity?>
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Entity);
+        }
+
+        public bool Equals(Entity? other)
+        {
+            return other is not null && Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public static bool operator ==(Entity? left, Entity? right)
+        {
+            return EqualityComparer<Entity>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Entity? left, Entity? right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ source/src/CarRent/Domain/Common/ValueObject.cs
@@ -0,0 +1,43 @@
+﻿namespace CarRent.Domain.Common
+{
+    public abstract class ValueObject : IEquatable<ValueObject?>
+    {
+        public ValueObject()
+        {
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as ValueObject);
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (GetType() != other.GetType())
+            {
+                return false;
+            }
+
+            return EqualityComponents.SequenceEqual(other.EqualityComponents);
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hashCode = default;
+
+            foreach (var obj in EqualityComponents)
+            {
+                hashCode.Add(obj);
+            }
+
+            return hashCode.ToHashCode();
+        }
+
+        protected abstract IEnumerable<object?> EqualityComponents { get; }
+    }
+}
